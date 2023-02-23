29ecd8fbf3dcd774ea229a6c8c13ae16c85a2834
keisuke-Hoshino <po258096@gmail.com>
2023-02-23 15:27:58 +0900

    lean prop-types

--- src/App.js
+++ src/App.js
@@ -1,28 +1,34 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-const App = ()  => {
-  const profiles = [
-      {name: "Taro",age:10},
-      {name: "Hanako",age:5},
-      {name: "Noname"}
-  ]
-  return (
-    <div>
-      {
-        profiles.map((profiles, index) => {
-          return <User name={profiles.name} age={profiles.age} key={index}/>
-        })
-      }
-    </div>
-  )
-}
+const App = () => {
+    const profiles = [
+        { name: "Taro", age: 10 },
+        { name: "Hanako", age: 5 },
+        { name: "NoName",age:12},
+    ];
+    return (
+        <div>
+            {profiles.map((profiles, index) => {
+                return (
+                    <User name={profiles.name} age={profiles.age} key={index} />
+                );
+            })}
+        </div>
+    );
+};
 
-const User = (props) =>{
-  return <div>Hi, I am {props.name}! and {props.age} years old</div>
-}
+const User = (props) => {
+    return (
+        <div>
+            Hi, I am {props.name}! and {props.age} years old
+        </div>
+    );
+};
 
-User.defaultProps = {
-  age:1
-}
+User.propTypes = {
+    name: PropTypes.string,
+    age: PropTypes.number.isRequired,
+};
 
 export default App;
